4db7338624687961a1de8ade2241e9db75fc8ad7
roan b <roman7tati@gmail.com>
2022-09-01 22:46:23 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+double[,] numbers = new double[rows, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine("");
+    }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine());
+double [] newArray = new double[columns];
+int[,] array = new int[rows, columns];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(-100, 100);
+        Console.Write(array[i, j] + "\t");
+        newArray [j] += Convert.ToDouble(array[i,j])/array.GetLength(0);
+    }
+    Console.WriteLine();
+}
+Console.Write(" ");
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    Console.Write(Math.Round(newArray[i],2) + "; ");
+}
